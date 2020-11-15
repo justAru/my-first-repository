eb4d91268418b9a9d3235e1cf5ccf5fe39b1cd79
Aru <a.demeubaeva@yandex.com>
2020-11-16 01:19:29 +0600

    Initial Commit

---
+++ realmProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realmProj
+//
+//  Created by Aru on 11/16/20.
+//  Copyright © 2020 Aruzhan Demeubayeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
