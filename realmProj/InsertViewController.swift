7adb1bca72a901865359cbdb9eee7b890ef5030d
Aru <a.demeubaeva@yandex.com>
2020-11-17 08:32:25 +0600

    first commit

---
+++ realmProj/InsertViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InsertViewController.swift
+//  realmProj
+//
+//  Created by Aru on 11/17/20.
+//  Copyright © 2020 Aruzhan Demeubayeva. All rights reserved.
+//
+
+import UIKit
+
+class InsertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
